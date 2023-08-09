fe2fd9c115a70045530be0adcc30de35e2f4c486
effham.ali <athereffham@gmail.com>
2023-08-09 23:05:12 +0500

    Major race work and check availibility api

--- app/Http/Controllers/RacesController.php
+++ app/Http/Controllers/RacesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Races;
+use Carbon\Carbon;
 
 class RacesController extends Controller
 {
@@ -29,16 +30,87 @@ class RacesController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'start_time' => 'required',
+            'start_time' => 'required|date_format:h:i A',
             'winner_horse_no' => 'required',
-            'started' => 'required', 
         ]);
-        return $bids = Races::create([
-            'start_time' => $request->start_time,
+
+        $proposedStartTime = \Carbon\Carbon::createFromFormat('h:i A', $request->start_time);
+
+        $existingSlots = Races::whereDate('created_at', '=', now()->format('Y-m-d'))->count();
+
+        if ($existingSlots > 0) {
+
+            $existingSlot = Races::whereDate('created_at', '=', now()->format('Y-m-d'))
+                ->where('start_time', $proposedStartTime->format('h:i A'))
+                ->exists();
+
+            if ($existingSlot) {
+                return response()->json([
+                    'error' => 'Cannot book slot. Another slot is already booked at the proposed start time.'
+                ], 422);
+            }
+
+            $existingSlotTimes = Races::whereDate('created_at', '=', now()->format('Y-m-d'))
+                ->pluck('start_time')->map(function ($time) {
+                    return \Carbon\Carbon::createFromFormat('h:i A', $time);
+                });
+
+            $validSlot = $existingSlotTimes->contains(function ($time) use ($proposedStartTime) {
+                return $time->diffInMinutes($proposedStartTime) === 60;
+            });
+
+            if (!$validSlot) {
+                return response()->json([
+                    'error' => 'Cannot book slot. The slot should be exactly 1 hour before or 1 hour after an existing slot.'
+                ], 422);
+            }
+        }
+
+        $bids = Races::create([
+            'start_time'      => $proposedStartTime->format('h:i A'),
             'winner_horse_no' => $request->winner_horse_no,
-            'started' => $request->started,
+        ]);
+
+        return response()->json([
+            'bids' => $bids
+        ], 201);
+    }
+
+
+    public function checkRaceAvailability(Request $request)
+    {
+        $request->validate([
+            'datetime' => 'required|string',
+        ]);
+
+        $requestedDatetime = Carbon::parse($request->datetime);
+
+        $requestedTime = $requestedDatetime->format('h:i A');
+
+        $existingRace = Races::whereDate('created_at', '=', $requestedDatetime->format('Y-m-d'))
+            ->where(function ($query) use ($requestedTime) {
+                $query->whereBetween('start_time', [
+                    Carbon::parse($requestedTime)->subMinutes(5)->format('h:i A'),
+                    Carbon::parse($requestedTime)->addMinutes(5)->format('h:i A'),
+                ])->orWhere('start_time', $requestedTime);
+            })->first();
+
+        // dd()
+        if ($existingRace) {
+            return response()->json([
+                'message' => 'Race is available on the specified time.',
+                'winner_horse_no' => $existingRace->winner_horse_no,
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'No race available on the specified time within the next 5 minutes.',
+            'winner_horse_no' => null,
         ]);
     }
+
+
+
 
     /**
      * Display the specified resource.
