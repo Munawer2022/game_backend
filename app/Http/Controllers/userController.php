f2961eefdeab9d6146221993ac2e7b44731b135c
Munawer <117267127+Munawer2022@users.noreply.github.com>
2023-08-10 16:24:46 +0500

    first commit

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -74,7 +74,7 @@ class userController extends Controller
             'user' => 'login success',
             'status' => 'success',
             'token' => $token,
-            // 'name' => $request->name,
+            'name' => $user->name,
             'mobile_no' => $request->mobile_no,
             'id' => $user->id,
             'code' => $user->code,
